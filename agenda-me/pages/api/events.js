c0f18626cfccc666c4e56ef7a0ce57a6f198a237
rodrigoFerreir <rodrigoferreirasilva1997@hotmail.com>
2021-11-25 18:45:10 -0300

    cors

--- agenda-me/pages/api/events.js
+++ agenda-me/pages/api/events.js
@@ -4,7 +4,8 @@ export default async function (req, res) {
     const { start } = req.query
     const events = await getEventsData(start)
 
-    res.send({
+
+    res.header("Access-Control-Allow-Origin", "*").send({
         quant: events.length,
         events
     })
