aece946a97bca0d52b4fd494c0634cc7fc9cb9d5
rodrigoFerreir <rodrigoferreirasilva1997@hotmail.com>
2021-11-25 20:10:32 -0300

    cors

--- agenda-me/pages/api/events.js
+++ agenda-me/pages/api/events.js
@@ -1,11 +1,17 @@
+import NextCors from 'nextjs-cors';
 import { getEventsData } from "../../lib/notion";
 
 export default async function (req, res) {
     const { start } = req.query
     const events = await getEventsData(start)
 
+    await NextCors(req, res, {
+        methods: ['GET'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
 
-    res.header("Access-Control-Allow-Origin", "*").send({
+    return res.send({
         quant: events.length,
         events
     })
